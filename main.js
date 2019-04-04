943095090b0b57a8c90d5d890e920a40abc7c10c
Andrey <andreypikalov13@gmail.com>
2019-04-04 22:09:47 +0300

    2
    2

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if ("serviceWorker" in navigator) {
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("pwabuilder-sw.js")
+      .register("service-worker.js")
       .then(function (reg) {
         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
       });
