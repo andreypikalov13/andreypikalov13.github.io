204828a1793a3f8f13d19807f25ca57eed68857e
Andrey <andreypikalov13@gmail.com>
2019-04-04 22:24:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 window.addEventListener('DOMContentLoaded',()=>{
 
 	
- if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("[PWA Builder] active service worker found, no need to register");
+  } else {
+    // Register the service worker
     navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+      .register("service-worker.js")
+      .then(function (reg) {
+        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+      });
   }
-})();
+} 
 
 
 
