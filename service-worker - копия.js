05e1b6f08238a31a54c4a7f07521a7047609413a
Andrey <andreypikalov13@gmail.com>
2019-04-05 11:42:19 +0300

    5
    5

---
+++ service-worker - копия.js
@@ -0,0 +1,73 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-final-1';
+var cached_urls = [
+  './index.html',
+  './style.css',
+  './data.json',
+  './main.js',
+  './sound/end.mp3',
+  './sound/timer.mp3',
+  './images/play.img',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cache_name)
+    .then(function(cache) {
+      return cache.addAll(cached_urls);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          if (response.status === 404) {
+            return caches.match('index.html');
+          }
+          return caches.open(cached_urls).then(function(cache) {
+           cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        console.log('Error, ', error);
+        return caches.match('index.html');
+      })
+    );
+  });
--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,57 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This is the "Offline page" service worker
 
-var dataCacheName = 'weatherData-v1';
-var cacheName = 'weatherPWA-final-1';
-var cached_urls = [
-  './index.html',
-  './style.css',
-  './data.json',
-  './main.js',
-  './sound/end.mp3',
-  './sound/timer.mp3',
-  './images/play.img',
-];
+const CACHE = "pwabuilder-page";
 
-self.addEventListener('install', function(event) {
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "index.html";
+
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
   event.waitUntil(
-    caches.open(cache_name)
-    .then(function(cache) {
-      return cache.addAll(cached_urls);
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+
+      if (offlineFallbackPage === "index.html") {
+        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      }
+
+      return cache.add(offlineFallbackPage);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+// If any fetch fails, it will show the offline page.
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(function (error) {
+      // The following validates that the request was for a navigation to a new document
+      if (
+        event.request.destination !== "document" ||
+        event.request.mode !== "navigate"
+      ) {
+        return;
+      }
+
+      console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+      return caches.open(CACHE).then(function (cache) {
+        return cache.match(offlineFallbackPage);
+      });
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(function(response) {
-          if (response.status === 404) {
-            return caches.match('index.html');
-          }
-          return caches.open(cached_urls).then(function(cache) {
-           cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-      }).catch(function(error) {
-        console.log('Error, ', error);
-        return caches.match('index.html');
-      })
-    );
+// This is an event that can be fired from your page to tell the SW to update the offline page
+self.addEventListener("refreshOffline", function () {
+  const offlinePageRequest = new Request(offlineFallbackPage);
+
+  return fetch(offlineFallbackPage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
+    });
   });
+});
