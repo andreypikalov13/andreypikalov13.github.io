d68ffce2d085cdce77bc45aed1251b607ef2278a
Andrey <andreypikalov13@gmail.com>
2019-04-08 14:26:46 +0300

    9
    9

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ window.addEventListener('DOMContentLoaded',()=>{
 //fg
 
 */
- 
+ UpUp.start({
+      'content-url': 'index.html' // show this page to offline users
+    });
 
 	var buttonChange = document.querySelectorAll('.program'),
 infoblock = document.querySelectorAll('.info > span'),
